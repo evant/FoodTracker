8efab6bc91ea3d1bb919ffbbf9b3a1d8925adf5d
Evan Tatarka <evan.tatarka@willowtreeapps.com>
2018-11-21 09:27:38 -0500

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Evan Tatarka on 11/21/18.
+//  Copyright © 2018 WillowTree. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
